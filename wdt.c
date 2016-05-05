c271be04f22542f2d8791fe7e6fcebd1da45eb65
Ben Collins <bcollins@maclara-llc.com>
2016-05-04 23:54:21 -0400

    Rework module release
    Signed-off-by: Ben Collins <bcollins@maclara-llc.com>

--- wdt.c
+++ wdt.c
@@ -10,13 +10,11 @@
  * of the License.
  */
 
-// #define DEBUG 1
-
 /* Helper to format our pr_* functions */
 #define pr_fmt(__fmt) KBUILD_MODNAME ": " __fmt
 
 #include <linux/module.h>
-#include <linux/version.h>
+#include <linux/device.h>
 #include <linux/sched.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
@@ -40,7 +38,6 @@ MODULE_AUTHOR("Paul DeMetrotion");
 static int cdev_major;      // 0 = allocate dynamically
 module_param(cdev_major, uint, S_IRUGO);
 MODULE_PARM_DESC(cdev_major, "Set the major number for wdt device (0 = auto, default)");
-static int wdt_major;
 
 // wdt char device structure
 static struct cdev wdt_cdev;
@@ -52,6 +49,8 @@ MODULE_PARM_DESC(base_port, "Set the base port for the wdt device (default is 0x
 
 // mutex & spinlock
 static DEFINE_MUTEX(mtx);
+static struct class *wdt_class;
+static dev_t devno;
 
 //**********************************************************************
 //			DEVICE OPEN
@@ -59,14 +58,8 @@ static DEFINE_MUTEX(mtx);
 // called whenever a process attempts to open the device file
 static int device_open(struct inode *inode, struct file *file)
 {
-	if (base_port == 0) {
-		pr_warning("**** OPEN ATTEMPT on uninitialized port *****\n");
-		return -1;
-	}
-
 	pr_devel("device_open(%p)\n", file);
-
-	return SUCCESS;
+	return 0;
 }
 
 //**********************************************************************
@@ -75,7 +68,6 @@ static int device_open(struct inode *inode, struct file *file)
 static int device_release(struct inode *inode, struct file *file)
 {
 	pr_devel("device_release(%p,%p)\n", inode, file);
-
 	return 0;
 }
 
@@ -84,66 +76,37 @@ static int device_release(struct inode *inode, struct file *file)
 //**********************************************************************
 static long device_ioctl(struct file *file, unsigned int ioctl_num, unsigned long ioctl_param)
 {
-	int ret_val;
+	int ret = 0;
 
 	pr_devel("IOCTL call Code %04X\n", ioctl_num);
 
-	// Switch according to the ioctl called
 	switch (ioctl_num) {
 	case IOCTL_READ_WDT:
-		ret_val = inb(base_port + 2);
-
-		pr_devel("Reading WDT Port %04x -> %02x\n", base_port + 2, ret_val);
-
-		return ret_val;
+		ret = inb(base_port + 2);
 
 	case IOCTL_WRITE_WDT:
-		// obtain lock before writing
 		mutex_lock_interruptible(&mtx);
-
-		ret_val = ioctl_param & 0xff;
-
-		outb(ret_val, base_port + 2);
-
-		//release lock
+		outb(ioctl_param & 0xff, base_port + 2);
 		mutex_unlock(&mtx);
-
-		pr_devel("Writing to WDT Port %04x <- %02x\n", base_port + 2, ret_val);
-
-		return SUCCESS;
+		break;
 
 	case IOCTL_SET_WDT_SEC:
-		// obtain lock before writing
 		mutex_lock_interruptible(&mtx);
-
 		outb(0x80, base_port + 1);
-
-		//release lock
 		mutex_unlock(&mtx);
-
-		pr_devel("Setting WDT to Seconds\n");
-
-		return SUCCESS;
+		break;
 
 	case IOCTL_SET_WDT_MIN:
-		// obtain lock before writing
 		mutex_lock_interruptible(&mtx);
-
 		outb(0, base_port + 1);
-
-		//release lock
 		mutex_unlock(&mtx);
+		break;
 
-		pr_devel("Setting WDT to Minutes\n");
-
-		return SUCCESS;
-
-	// Catch all return
 	default:
-		return(-EINVAL);
+		ret = -EINVAL;
 	}
 
-	return SUCCESS;
+	return ret;
 }
 
 //**********************************************************************
@@ -161,75 +124,68 @@ static struct file_operations wdt_fops = {
 //**********************************************************************
 //			INIT MODULE
 //**********************************************************************
-// register the character device
 int init_module()
 {
 	int ret_val;
-	dev_t devno;
 
-	// Sign-on
-	pr_info(MOD_DESC " loading\n");
+	wdt_class = class_create(THIS_MODULE, KBUILD_MODNAME);
+	if (IS_ERR(wdt_class)) {
+		pr_err("Could not create module class\n");
+		return PTR_ERR(wdt_class);
+	}
 
-	// register the character device
+	/* Register the character device. */
 	if (cdev_major) {
-		wdt_major = cdev_major;
-		devno = MKDEV(wdt_major, 0);
+		devno = MKDEV(cdev_major, 0);
 		ret_val = register_chrdev_region(devno, 1, KBUILD_MODNAME);
 	} else {
 		ret_val = alloc_chrdev_region(&devno, 0, 1, KBUILD_MODNAME);
-		wdt_major = MAJOR(devno);
 	}
 
 	if (ret_val < 0) {
-		pr_err("Cannot obtain major number %d\n", wdt_major);
-		return -ENODEV;
-	} else {
-		pr_info("Major number %d assigned\n", wdt_major);
+		pr_err("Could not allocate character device\n");
+		goto err_del_class;
 	}
 
-	// add character device
+	/* Add character device. */
 	cdev_init(&wdt_cdev, &wdt_fops);
-	wdt_cdev.owner = THIS_MODULE;
-	wdt_cdev.ops = &wdt_fops;
-	ret_val = cdev_add(&wdt_cdev, MKDEV(wdt_major, 0), 1);
-
-	if (!ret_val) {
-		pr_info("Added character device\n");
-	} else {
-		pr_err("Error %d adding character device\n", ret_val);
-		goto exit_majnum_delete;
+	ret_val = cdev_add(&wdt_cdev, devno, 1);
+	if (ret_val) {
+		pr_err("Error adding character device\n");
+		goto err_rel_chrdev;
 	}
 
-	// check and map our I/O region requests
+	/* Check and map our I/O region requests. */
 	if (request_region(base_port, 4, KBUILD_MODNAME) == NULL) {
 		pr_err("Unable to use I/O Address %04X\n", base_port);
-		goto exit_cdev_delete;
-	} else {
-		pr_info("Base I/O Address = %04X\n", base_port);
+		ret_val = -ENOMEM;
+		goto err_rel_chrdev;
 	}
 
-	return SUCCESS;
+	pr_info("Added device @ 0x%04x\n", base_port);
 
-exit_cdev_delete:
+	device_create(wdt_class, NULL, devno, NULL, "%x", base_port);
+
+	return 0;
+
+err_rel_chrdev:
 	cdev_del(&wdt_cdev);
-exit_majnum_delete:
 	unregister_chrdev_region(devno, 1);
-	wdt_major = 0;
-	return -ENODEV;
+err_del_class:
+	class_destroy(wdt_class);
+
+	return ret_val;
 }
 
 //**********************************************************************
 //			CLEANUP MODULE
 //**********************************************************************
-// unregister the appropriate file from /proc
 void cleanup_module()
 {
-	// unregister I/O port usage
+	device_destroy(wdt_class, devno);
+	class_destroy(wdt_class);
+	unregister_chrdev_region(devno, 1);
 	release_region(base_port, 4);
 
-	// remove and unregister the device
-	cdev_del(&wdt_cdev);
-
-	unregister_chrdev_region(MKDEV(wdt_major, 0), 1);
-	wdt_major = 0;
+	pr_info("Removed device\n");
 }
